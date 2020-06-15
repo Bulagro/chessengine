ba9d3ce5a734064ebfb97f6dc467d5278acc0b17
Bulagro <christumer@gmail.com>
2020-06-15 13:35:38 -0500

    Redo main engine logic.
    Each piece is a now an instance of <Piece>.
    The piece itself only holds information about it's type (name) and the
    team it belongs to.
    That information can be retrieved with <Piece>.get()
    
    Pieces' movement is now handled by <Board>.
    Each piece has it's own movement function.
    To simplify the moving process, <Board>.move_piece() exists:
       it looks up move_map, a dictionary that maps a name with it's
       relative function.
    
    Changes summary:
    - Created PieceEnum enum: Holds pieces' names.
    - <Piece> is now just a @dataclass.
    - Each piece is an instance of <Piece>.
    - <Board>.pieces isn't a placeholder anymore.
    - <Board>.get_moves() modified to return <Piece>.get() if possible.
    - <Board> now contains the functions to get each piece's moves.

--- engine.py
+++ engine.py
@@ -1,94 +1,130 @@
 from enum import Enum, auto
 
-
-class Board:
-    def __init__(this):
-        this.set()
-
-    def set(this):
-        # This is a placeholder :)
-        this.pieces = [
-            [Rook(Team.WHITE, this), 'WKnight', 'WBishop', 'WQueen', 'WKing', 'WBishop', 'WKnight', Rook(Team.WHITE, this)],
-            [Pawn(Team.WHITE, this)] * 8
-            [None] * 8,
-            [None] * 8,
-            [None] * 8,
-            [None] * 8,
-            [Pawn(Team.BLACK, this)] * 8
-            [Rook(Team.BLACK, this), 'BKnight', 'BBishop', 'BQueen', 'BKing', 'BBishop', 'BKnight', Rook(Team.BLACK, this)],
-        ]
-
-    def get_piece(this, x, y):
-        if not (0 <= x <= 7 and 0 <= y <= 7):
-            return False
-
-        return this.pieces[y][x]
-
-
 class Team(Enum):
     WHITE = auto()
     BLACK = auto()
 
+class PieceEnum(Enum):
+    PAWN =   auto()
+    ROOK =   auto()
+    KNIGHT = auto()
+    BISHOP = auto()
+    QUEEN =  auto()
+    KING =   auto()
+    
+class Board:
+    def __init__(this):
+        this.set()
+        this.move_map = {
+            PieceEnum.PAWN : this.get_pawn_moves,
+            PieceEnum.ROOK : this.get_rook_moves,
+            None : [],
+            }
 
-class Piece:
-    def __init__(this, team, board):
-        this.team = team
-        this.board = board
+    def set(this):
+        # This is NOT a placeholder :)
+        this.pieces = [
+            [
+            Piece(PieceEnum.ROOK,   Team.WHITE),
+            Piece(PieceEnum.KNIGHT, Team.WHITE),
+            Piece(PieceEnum.BISHOP, Team.WHITE),
+            Piece(PieceEnum.QUEEN,  Team.WHITE),
+            Piece(PieceEnum.KING,   Team.WHITE),
+            Piece(PieceEnum.BISHOP, Team.WHITE),
+            Piece(PieceEnum.KNIGHT, Team.WHITE),
+            Piece(PieceEnum.ROOK,   Team.WHITE),            
+            ],
+            [Piece(PieceEnum.PAWN, Team.WHITE)] * 8,
+            [None] * 8,
+            [None] * 8,
+            [None] * 8,
+            [None] * 8,
+            [Piece(PieceEnum.PAWN, Team.BLACK)] * 8,
+            [
+            Piece(PieceEnum.ROOK,   Team.WHITE),
+            Piece(PieceEnum.KNIGHT, Team.WHITE),
+            Piece(PieceEnum.BISHOP, Team.WHITE),
+            Piece(PieceEnum.QUEEN,  Team.WHITE),
+            Piece(PieceEnum.KING,   Team.WHITE),
+            Piece(PieceEnum.BISHOP, Team.WHITE),
+            Piece(PieceEnum.KNIGHT, Team.WHITE),
+            Piece(PieceEnum.ROOK,   Team.WHITE),            
+            ],
+        ]
 
-    def get_moves(this):
-        # Place holder, must be implemented individually per piece type
-        pass
+    def get_piece(this, x: int, y: int):
+        if not (0 <= x <= 7 and 0 <= y <= 7):
+            return False
 
+        piece = this.pieces[y][x]
+        return None if not piece else piece.get()
 
-class Pawn:
-    def __init__(this, team, board):
-        super.__init__(team, board)
-        this.has_moved = False
-        this.forward = 1 if team == Team.BLACK else -1
+    def get_pawn_moves(this, x: int, y: int):
+        if p := this.get_piece(x, y):
+            forward = -1 if p['team'] == Team.BLACK else 1
+            has_moved = (y != (6 if p['team'] == Team.BLACK else 1))
+            output = []
 
-    def get_moves(this, x: int, y: int):
-        output = []
-
-        # Forward one square
-        aux = (x, y + this.forward)
-        if this.board.get_piece(aux[0], aux[1]) is None:
-            output.append(aux)
-
-            # Forward two sqwares
-            aux = (x, y + 2 * this.forward)
-            if (not this.has_moved) and this.board.get_piece(
-                    aux[0], aux[1]) is None:
+            # Forward one square
+            aux = (x, y + forward)
+            if this.get_piece(aux[0], aux[1]) is None:
                 output.append(aux)
 
-        # Attack diagonal right
-        aux = (x + 1, y + this.forward)
-        target = this.board.get_piece(aux[0], aux[1])
-        if target and target.team != this.team:
-            output.append(aux)
+                # Forward two sqwares
+                aux = (x, y + 2 * forward)
+                if (not has_moved) and this.get_piece(
+                        aux[0], aux[1]) is None:
+                    output.append(aux)
 
-        # Attack diagonal left
-        aux = (x - 1, y + this.forward)
-        target = this.board.get_piece(aux[0], aux[1])
-        if target and target.team != this.team:
-            output.append(aux)
+            # Attack diagonal right
+            aux = (x + 1, y + forward)
+            target = this.get_piece(aux[0], aux[1])
+            if target and target['team'] != p['team']:
+                output.append(aux)
 
-        return output
+            # Attack diagonal left
+            aux = (x - 1, y + forward)
+            target = this.get_piece(aux[0], aux[1])
+            if target and target['team'] != p['team']:
+                output.append(aux)
 
+            return output
 
-class Rook(Piece):
-    def __init__(this, team, board):
-        super.__init__(team, board)
+        # Invalid pos (OOB)
+        return None
 
-    def get_moves(this, x, y):
-        output = []
-        for tempname in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
-            i = 1
-            while (target := this.board.get_piece(
-                (tx := x + tempname[0] * i),
-                (ty := y + tempname[1] * i))
-            ) is None:
-                i += 1
-                output.append((tx, ty))
-            else:
-                if target and target.team != this.team:
+    def get_rook_moves(this, x: int, y: int):
+        if p := this.get_piece(x, y):
+            output = []
+            for tempname in [[1, 0], [0, 1], [-1, 0], [0, -1]]:
+                i = 1
+                while (target := this.get_piece(
+                    (tx := x + tempname[0] * i),
+                    (ty := y + tempname[1] * i))
+                    ) is None:
+                    i += 1
                     output.append((tx, ty))
+                else:
+                    if target and target['team'] != p['team']:
+                        output.append((tx, ty))
+
+            return output
+        return None
+
+    def move_piece(this, x, y):
+        piece = this.get_piece(x ,y)
+
+        return move_map[piece[0]](x, y, piece[1]) if piece else []
+
+@dataclass
+class Piece:
+    def __init__(this, name: PieceEnum, team: Team):
+        this.name = name
+        this.team = team
+
+    def get(this):
+        return {
+            'name' : this.name,
+            'team' : this.team,
+            }
+
