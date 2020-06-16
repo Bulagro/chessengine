0b17476d7b795cc7ea9d68c1f90ef90164c5d0b4
Bulagro <christumer@gmail.com>
2020-06-15 19:22:21 -0500

    Custom board layout function for testing.
    You give it a string with a representative code and it transformes it
    into a proper board.
    
    Each piece is represented by a character, where uppercase stands for
    team WHITE, and lowercase for BLACK:
    - k/K -> King
    - q/Q -> Queen
    - b/B -> Bishop
    - n/N -> Knight
    - r/R -> Rook
    - p/P -> Pawn
    -  .  -> Nothing

--- engine_test.py
+++ engine_test.py
@@ -1,13 +1,54 @@
 import engine
 import unittest
 
+
+def custom_board(placement: str):
+    pieces_dict = {
+        # Upper case for white team
+        'K': engine.Piece(engine.PieceEnum.KING, engine.Team.WHITE),
+        'Q': engine.Piece(engine.PieceEnum.QUEEN, engine.Team.WHITE),
+        'B': engine.Piece(engine.PieceEnum.BISHOP, engine.Team.WHITE),
+        'N': engine.Piece(engine.PieceEnum.KNIGHT, engine.Team.WHITE),
+        'R': engine.Piece(engine.PieceEnum.ROOK, engine.Team.WHITE),
+        'P': engine.Piece(engine.PieceEnum.PAWN, engine.Team.WHITE),
+
+        # Lower case for black team
+        'k': engine.Piece(engine.PieceEnum.KING, engine.Team.BLACK),
+        'q': engine.Piece(engine.PieceEnum.QUEEN, engine.Team.BLACK),
+        'b': engine.Piece(engine.PieceEnum.BISHOP, engine.Team.BLACK),
+        'n': engine.Piece(engine.PieceEnum.KNIGHT, engine.Team.BLACK),
+        'r': engine.Piece(engine.PieceEnum.ROOK, engine.Team.BLACK),
+        'p': engine.Piece(engine.PieceEnum.PAWN, engine.Team.BLACK),
+
+        '.': None,
+    }
+    placement = placement.replace(' ', '').replace('\n', '').replace('\t', '')
+    valid_pieces = tuple('KkQqBbNnRrPp.')
+
+    i = 0
+    board, line = [], []
+    while i < len(placement):
+        if placement[i] in valid_pieces:
+            line.append(pieces_dict[placement[i]])
+
+        if len(line) == 8:
+            board.append(line)
+            line = []
+
+        i += 1
+
+    board.append(line)
+
+    return board
+
+
 class TestPawnMoves(unittest.TestCase):
     def test_pawn_first_move(this):
         B = engine.Board()
 
         for i in range(8):
             # White
-            expected_moves = [(i, 2), (i,3)]
+            expected_moves = [(i, 2), (i, 3)]
             actual_moves = B.get_pawn_moves(i, 1)
 
             this.assertEqual(expected_moves, actual_moves)
